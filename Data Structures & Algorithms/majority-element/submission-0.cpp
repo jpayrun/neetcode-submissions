2a8dfbf31dbe57bd46782f54754f2641b154d089
Jordan <27322284+jpayrun@users.noreply.github.com>
2026-05-31 10:18:46 -0400

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> vals;
+        for (const auto &num : nums) {
+            if (vals.count(num) == 0) vals[num] = 1;
+            else vals[num]++;
+        }
+        int m_key = 0;
+        int res;
+        for (const auto [key, val] : vals) {
+            if (vals[key] > m_key) {
+                res = key;
+                m_key = vals[key];
+            }
+        }
+        return res;
+    }
+};
