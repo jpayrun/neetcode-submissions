466df8a0dde60f84dea5a09599d76a7a87a59eec
Jordan <27322284+jpayrun@users.noreply.github.com>
2026-06-07 17:55:33 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> res;
+    vector<string> generateParenthesis(int n) {
+        string path = "";
+        bt(n, n, 0, path);
+        return res;
+    }
+    void bt(int left, int right, int count, string &path) {
+        if (left == 0 && right == 0) {
+            res.push_back(path);
+            return;
+        }
+        if (left > 0) {
+            path.push_back('(');
+            bt(left - 1, right, count + 1, path);
+            path.pop_back();
+        }
+        if (right > 0 && count > 0) {
+            path.push_back(')');
+            bt(left, right - 1, count - 1, path);
+            path.pop_back();
+        }
+    }
+};
