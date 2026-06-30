07b2da78cd4679411b193ca9cf51b57b2059b184
Jordan <27322284+jpayrun@users.noreply.github.com>
2026-06-29 20:58:19 -0400

    Add: stone-game - submission-3

---
+++ Data Structures & Algorithms/stone-game/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    map <vector<int>, int> memo;
+    bool stoneGame(vector<int>& piles) {
+        int res = dfs(0, piles.size() - 1, piles);
+        int tot = accumulate(piles.begin(), piles.end(), 0);
+        return res > tot / 2;
+    }
+    int dfs(int l, int r, vector<int> &piles) {
+        if (l > r) return 0;
+        if (memo.count({l, r}) == 1) return memo[{l, l}];
+        int left = piles[l] + dfs(l+1, r, piles);
+        int right = piles[r] + dfs(l, r-1, piles);
+        memo[{l, r}] = max(left, right);
+        return memo[{l, r}];
+    }
+};
