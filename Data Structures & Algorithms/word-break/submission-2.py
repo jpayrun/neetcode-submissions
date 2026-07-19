555d2de1c1d295eb253afe7efa70ac46b19836a2
Jordan <27322284+jpayrun@users.noreply.github.com>
2026-07-19 17:23:23 -0400

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+        def helper(s):
+            if s in memo:
+                return memo[s]
+            if not s:
+                return True
+            for word in wordDict:
+                if word == s[:len(word)]:
+                    memo[s] = helper(s[len(word):])
+                    if memo[s]:
+                        return memo[s]
+            return False
+        return helper(s)
