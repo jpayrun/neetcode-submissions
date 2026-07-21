38cb7a12683559a391c2bd13f3a592055b82a5be
Jordan <27322284+jpayrun@users.noreply.github.com>
2026-07-21 18:58:47 -0400

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        def diff(w1, w2):
+            return abs(len(set(w1)) - len(set(w2)))
+        def comp(w1, w2):
+            res = len(w1)
+            for c1, c2 in zip(w1, w2):
+                if c1 == c2:
+                    res-=1
+            return res
+        dist = {}
+        heap = []
+        heapq.heappush(heap, (1, 1, beginWord))
+        while heap:
+            time, _, word = heapq.heappop(heap)
+            print(f"{word} {time}")
+            if word == endWord:
+                return time
+            if word in dist and dist[word] < time:
+                continue
+            dist[word] = time
+            for w in wordList:
+                t = time + 1
+                if w in dist and dist[w] < t:
+                    continue
+                if comp(w, word) <= 1:
+                    d = diff(w, word)
+                    heapq.heappush(heap, (t, d, w))
+        return 0
