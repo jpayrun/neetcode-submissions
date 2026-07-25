f4ac1d7612945a032020cab415bc31e06a3c392b
Jordan <27322284+jpayrun@users.noreply.github.com>
2026-07-24 22:38:45 -0400

    Add: palindrome-number - submission-6

---
+++ Data Structures & Algorithms/palindrome-number/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        long long div = 1;
+        while (div <= x) div*=10;
+        div/=10;
+        while (x > 0) {
+            if (x % 10 != x / div) return false;
+            x = (x % div) / 10;
+            div/=100;
+        }
+        return true;
+    }
+};
