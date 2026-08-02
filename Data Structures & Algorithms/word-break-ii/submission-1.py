070f3f64e31f6b5cb6cb61315d59550deb879e26
Jordan <27322284+jpayrun@users.noreply.github.com>
2026-08-01 20:22:22 -0400

    Add: word-break-ii - submission-1

---
+++ Data Structures & Algorithms/word-break-ii/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # memo = {}
+        self.res = []
+        def helper(s, path):
+            key = (s, path)
+            if not s:
+                tmp = ' '.join(path)
+                self.res.append(tmp)
+                return
+            for word in wordDict:
+                if word == s[:len(word)]:
+                    path.append(word)
+                    helper(s[len(word):], path)
+                    path.pop()
+            return
+        helper(s, [])
+        return self.res
