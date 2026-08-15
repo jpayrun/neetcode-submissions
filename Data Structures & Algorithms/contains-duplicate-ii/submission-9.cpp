27769a33f51d83c789fc8a033b5af40f46e52183
Jordan <27322284+jpayrun@users.noreply.github.com>
2026-08-15 13:33:20 -0400

    Add: contains-duplicate-ii - submission-9

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-9.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int l = 0;
+        unordered_set <int> s;
+        for (int r = 0; r < nums.size(); r++) {
+            if (r - l > k) s.erase(nums[l++]);
+            if (s.count(nums[r])) return true;
+            s.insert(nums[r]);
+        }
+        return false;
+    }
+};
